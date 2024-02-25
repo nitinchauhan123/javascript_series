7348818c271ccb0662e79a7f0c2104050e4e4494
Nitin singh chauhan <89001732+nitinchauhan123@users.noreply.github.com>
2024-02-25 05:26:34 +0000

    🔥🔥

--- 01_Basic/01_Variables.js
+++ 01_Basic/01_Variables.js
@@ -2,4 +2,22 @@ const AccountId = 12345;
 let EmailId = "nitinchauhan@123.com";
 var AccountNumber = 1234;
 Address = "Ateli"
-console.log(AccountId);
+
+
+// AccountId = "123456"; Not allowed
+// console.log(AccountId);
+
+EmailId = "1234@.com";
+AccountNumber = "112345"
+Address = "kanti"
+
+let age;  // undefined 
+
+/*
+var prefer not to use var 
+because of issuse in block scope and function scope 
+
+
+*/
+
+console.table([EmailId,AccountNumber,Address,age])
---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,37 @@
+'use strict' // treat all js code as newer version
+
+// alert("Hello") // we are using nodejs not brower
+
+// alert(3+4)
+console.log("hello Nitin"); 
+
+console.log(3+
+    4); // code readability should be high
+
+
+    // data types 
+    // let name = "Nitin";
+    // let age = 26;
+    // let isLoggeDin = true / false
+
+// 1 Number in range => 2 to power 53
+
+// 2 String
+// 3 Boolean 
+// 4 null
+// 5 BigInt
+// 6 Symbol
+// 7 undefined
+
+
+
+// Bigint
+// string => ""
+// boolean => true / false
+// null // empty value
+// symbol // unique value 
+// undefined 
+ 
+let score = null;  // typeof object 
+
+console.log( typeof score);
