6b56a9459fcd49a509116281fc83428b93169c12
Nitin singh chauhan <89001732+nitinchauhan123@users.noreply.github.com>
2024-02-25 11:02:47 +0000

    🔥🔥

---
+++ 01_Basic/03_datatypeConversion.js
@@ -0,0 +1,72 @@
+let score = true;
+
+
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+let valueINNumber = Number(score)
+
+
+// console.log( typeof valueINNumber);
+// console.log(valueINNumber);
+
+// console.log(typeof NaN);
+
+ 
+// "33" => 33
+// "33abc" = NaN
+// true => 1 false => 0
+
+
+let login = "nitin";
+
+let results = Symbol(login);
+
+// console.log(typeof results);
+// console.log(results);
+
+
+// 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+// string to number conversion
+
+// let value = 3
+
+// let negValue = -value
+
+// console.log(negValue);
+
+
+
+// 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+// opertions js 
+
+// console.log(2+2);
+// console.log(2-4);
+// console.log(2*3);
+// console.log(2**5);
+// console.log(2/5);
+// console.log(2%6);
+
+// console.log("1"+ 2);
+// console.log(2 + "3");
+// console.log("1"+3 + 4);
+// console.log(2+4+"4");
+
+// console.log(+true);
+
+
+// console.log(+"");
+
+
+
+let gameCounter =100;
+
+// ++gameCounter;
+--gameCounter
+console.log(gameCounter);
+
+
+
+
---
+++ 01_Basic/04_Comparion.js
@@ -0,0 +1,72 @@
+// console.log(2>1);
+// console.log(3 <1);
+// console.log(3 >= 4);
+// console.log(2==4);
+// console.log(4 !== 9);
+
+
+// console.log("2" >= 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+
+
+// console.log("2"=== 3);
+
+
+/// Summary data types 
+
+// primitive value  // non primitive data types 
+
+// yeh call by value hote hai 
+
+// 7 types : 
+// 1 string
+// 2 number
+// 3 boolean
+// 4 undefined
+// 5 Symbol
+// 6 BigInt
+// 7 null
+
+// Non primitive data types 
+
+// function array object
+
+let id = Symbol('1234')
+let anotherId = Symbol('1234')
+
+// console.log(id===anotherId);
+
+const bigNumber = 762534127347126412n;
+
+// console.log(bigNumber);
+
+// stack heap in javascript 
+
+// stack 
+
+// // Primitive data types and references	Objects and functions
+// // Size is known at compile time 	Size is known at run time
+// // Fixed memory allocated
+
+// // heap
+
+// // object and function 
+// // size is known at run time
+// no limit for object memory
+
+
+
+// stack (primitive )data types use hoti hai 
+
+// stack memory ka copy diya jata hai 
+//  heap ke under refarance milta hai 
+// jo bhii change karte hai wo oringal value me change hota hai 
+
+console.log('Hello');
