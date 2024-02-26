4bd22bd1e62320591c70a0bbcd27ed24510b37e9
Nitin singh chauhan <89001732+nitinchauhan123@users.noreply.github.com>
2024-02-26 04:56:38 +0000

    🔥🔥

---
+++ 01_Basic/05_String.js
@@ -0,0 +1,47 @@
+// const name = "NitinSingh";
+// const repoCount = 50;
+
+// console.log(name + repoCount + "  value");
+
+// Template literals
+
+// console.log(`my name ${name} my github repoCount ${repoCount}`);
+
+// const GameName = new String("Nitin");
+
+
+let Str = "JavaScript is a Great  is Language"
+
+// let a = Str.length
+
+// let a = Str.toLowerCase();
+
+// let a = Str.includes("JavaScripst") // true / false return karta hai 
+
+// let a = Str.startsWith("Language") // yeh first word ko search karta hai 
+
+// let a = Str.endsWith("Language")
+
+// let a = Str.search("")
+
+// let a = Str.match(/is/g)
+
+// let a = Str.indexOf("is")
+
+
+// let a = Str.lastIndexOf("is")
+
+// let a = Str.replace("JaaScript","mpohan")
+// let a = Str.trim()
+
+// let a = Str.charAt(9)
+
+// let a = Str.split("s")
+
+// let a = Str.repeat(2)
+
+// let a = Str.slice(1,2) // yeh last value add nahi krta -value bhii de skte hai isme
+
+let a = Str.toString()
+
+console.log(a);
