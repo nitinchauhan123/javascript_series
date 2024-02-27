0617e568dcaa6f08ee6f11e9d6e555956a5bdbdb
Nitin singh chauhan <89001732+nitinchauhan123@users.noreply.github.com>
2024-02-27 05:31:24 +0000

    🔥🔥

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,42 @@
+// Array in js 
+let myArr = [0,1,2,3,4,5];
+
+// let myHero = ['shaktiman','naagraj']
+// const myArr2 = new Array(1,2,3,4,56,)
+
+// console.log(myArr[0]);
+
+// myArr.push(6);
+// console.log(myArr);
+
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift([1,23,4,]);
+
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr);
+
+
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(5));
+
+// let mynum = myArr.join();
+// console.log(myArr);
+
+// console.log( typeof mynum);
+
+// slice splice method in  javascript
+
+let num = myArr.slice(0,3) // last value add nahi hoti hai 
+
+console.log(num);
+
+console.log(myArr);
+
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 3, );
+console.log(months);
+
