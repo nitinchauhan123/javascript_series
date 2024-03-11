475121ec2a6b94a715a6db4dc6e572ec4d833979
Nitin singh chauhan <89001732+nitinchauhan123@users.noreply.github.com>
2024-03-11 14:29:28 +0000

    🔥🔥

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,40 +1,35 @@
-// for of
+// for of loop in javascript
 
-// ["", "", ""]
-// [{}, {}, {}]
 
-const arr = [1, 2, 3, 4, 5]
+// const Myarr = [0,1,2,3,4,5,6,7,8,9,10];
 
-for (const num of arr) {
-    //console.log(num);
-}
+// for (const Item of Myarr) {
+//     console.log(Item);
+// }
 
-const greetings = "Hello world!"
-for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
-}
+// const greetings = "Nitin Singh";
 
-// Maps
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
 
 const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")
-
+map.set("IN",'India')
+map.set("USA",'America')
+map.set("Fr",'France')
+map.set("IN",'India')
 
 // console.log(map);
 
-for (const [key, value] of map) {
-    // console.log(key, ':-', value);
-}
-
-const myObject = {
-    game1: 'NFS',
-    game2: 'Spiderman'
-}
-
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
+// for (const [key,value] of map) {
+//     console.log(key,"-",value);
 // }
+
+const user = {
+    "hello": "Nitin",
+    "Age": '23'
+};
+
+for (const[ item,value] of user) {
+    console.log(item,"-",value);
+}
--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,21 +1,23 @@
+// do-while loop while in javscript
 
-let index = 0
+// let index = 0;
 // while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
+//     console.log(`value of index ${index}`);
+//    index++;
+//    index = index + 2;
 // }
 
-let myArray = ['flash', "batman", "superman"]
+// let MyArr = ['rhaul','priya','mohan'];
 
-let arr = 0
-while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
-    arr = arr + 1
-}
+// let arr = 0;
+// while (arr < MyArr.length) {
+//     console.log(`value is ${MyArr[arr]}`);
+//     arr++;
+// }
 
-let score = 11
+let score = 11;
 
 do {
-    console.log(`Score is ${score}`);
-    score++
-} while (score <= 10);
+    console.log(`value is ${score}`);
+    score++;
+} while (score <=10);
